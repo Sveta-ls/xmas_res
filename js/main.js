e5f45da400e5fb564b7c7e77dc2df206b496f4a5
Sveta-ls <slana6570@gmail.com>
2025-12-28 00:01:47 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,40 @@
+fetch("data/info.json")
+  .then((res) => res.json())
+  .then((info) => {
+    renderInfoLeft("info-left-1", info[0]);
+    renderInfoRight("info-right-1", info[1]);
+    renderInfoLeft("info-left-2", info[2]);
+  });
+
+function renderInfoLeft(id, data) {
+  document.getElementById(id).innerHTML = `
+    <div class="info_left info_left_bg" style="background-image: url(${data.bk})">
+      <h1 class="info_l_h">${data.h}</h1>
+      <p class="info_l_p">${data.text}</p>
+    </div>
+  `;
+}
+
+function renderInfoRight(id, data) {
+  document.getElementById(id).innerHTML = `
+    <div class="info_right">
+      <div
+        class="info_right_bg"
+        style="background-image: url(${data.bk})"
+      >
+        <div class="info_right_content">
+          <h1 class="info_r_h">${data.h}</h1>
+          <p class="info_r_p">${data.text}</p>
+        </div>
+      </div>
+    </div>
+  `;
+}
+
+/* анимация клика */
+document.querySelectorAll(".click-img").forEach((img) => {
+  img.addEventListener("click", () => {
+    img.classList.add("ripple-animation");
+    setTimeout(() => img.classList.remove("ripple-animation"), 500);
+  });
+});
